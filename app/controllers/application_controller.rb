50bb439b4a3fbb67156c1e5f3e01ef0ecbecb6f6
Thierry Chau <thierry.chau@gmail.com>
2024-03-19 11:03:08 -0500

    Revert "Sj add punit"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  
   before_action :authenticate_user!
- 
-  after_action :verify_authorized, unless: :devise_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
-  include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -13,15 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :private, :name, :bio, :website, :avatar_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :private, :name, :bio, :website, :avatar_image])
   end
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-   private
-
-    def user_not_authorized
-      flash[:alert] = "You are not authorized to perform this action."
-      
-      redirect_back(fallback_location: root_path)
-    end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
-  before_action { authorize (@user || User )}
-
   private
 
     def set_user
@@ -12,10 +10,4 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
-
-    def must_be_owner_to_view
-      if current_user != @user
-        redirect_back fallback_location: root_url, alert: "You're not authorized"
-    end
-end
 end
